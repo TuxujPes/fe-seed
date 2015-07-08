8962a98544159bb0ff4dd3116bdbf30cd6ff95cd
Taras Kharuk <tuxuj.pes@yandex.ru>
2015-07-08 16:30:52 +0300

    improve dummy table

---
+++ app/scripts/configs/request_table_titles_map.js
@@ -0,0 +1,10 @@
+export default {
+  title: 'Project Title',
+  idx: 'ID #',
+  submitted: 'Submitted',
+  updated: 'Updated',
+  keydate: 'Keydate',
+  classification: 'Classification',
+  status: 'Status',
+  comments: 'Comments'
+};
