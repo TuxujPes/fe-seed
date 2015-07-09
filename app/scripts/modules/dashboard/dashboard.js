99c6038b253d378ef662101daa70b73db4ffd179
banzay <yuriy.zdyrko@gmail.com>
2015-07-09 10:56:21 +0300

    small changes to dashboard

--- app/scripts/modules/dashboard/dashboard.js
+++ app/scripts/modules/dashboard/dashboard.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import NavigationRegion from './navigation_region';
 import AtAGlance from './ataglance';
-
-const main = 'dashboard';
-const overviewRegion = main + '__overview-region';
-const overview = main + '__overview';
-const header = main + '__header';
-const infoWrapper = main + '__info';
-const infoParagraph = infoWrapper + '__paragraph';
-const bulletinBoard = 'bulletin-board';
+import MainMenu from './main_menu';
 
 export default class DashboardPage extends React.Component {
   render() {
     return (
-      <div className={main}>
-        <div className={overviewRegion}>
-          <div className={overview}>
-            <h2 className={header}>Welcome to the Naming Center</h2>
-            <div className={infoWrapper}>
-              <p className={infoParagraph}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              <p className={infoParagraph}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <div className="dashboard">
+        <MainMenu />
+        <div>asdfasdf</div>
+        <div className="dashboard__overview">
+          <div className="dashboard__overview__text">
+            <h1 className="dashboard__overview__text__header">Welcome to the Naming Center</h1>
+            <div className="dashboard__overview__text__content">
+              <p className="dashboard__overview__text__content__item">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p className="dashboard__overview__text__content__item">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
           </div>
-          <div className={bulletinBoard}>
-            BULLETINBOARD
+          <div className="dashboard__bulletins">
+            BULLETINBOARDDD
+            BULLETINBOARDDDDaasdasdfasdf
           </div>
         </div>
         <NavigationRegion />
---
+++ app/scripts/modules/dashboard/main_menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class MainMenu extends React.Component {
+  render() {
+    return (
+      <div className="main-menu">
+        <div className="main-menu__item">Our Brands</div>
+        <div className="main-menu__item">Brands Elements</div>
+        <div className="main-menu__item">Applying Your Brand</div>
+        <div className="main-menu__item">Naming Center</div>
+      </div>
+    )
+  }
+}
+
+export default MainMenu;
